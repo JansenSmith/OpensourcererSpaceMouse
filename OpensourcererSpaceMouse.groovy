a350fcd42ad499c9d2ef6a6f16f6e2c2d70f4e5c
JansenSmith <jansen@bstarengineering.com>
2024-05-08 23:14:40 -0400

    got a start

--- OpensourcererSpaceMouse.groovy
+++ OpensourcererSpaceMouse.groovy
@@ -2,12 +2,14 @@
 // it is how we can have totally cloud based robots. 
 //You can load files directly from git
 // you can cast the object you get back and use it in your script like any other object. 
-CSG ref = ScriptingEngine.gitScriptRun(
-	                "https://github.com/JansenSmith/OpensourcererSpaceMouse.git", // git location of the library
-	                      "v2-frame.stl" , // file to load
-	                      null)// no parameters (see next tutorial)
+//CSG ref = ScriptingEngine.gitScriptRun(
+//	                "https://github.com/JansenSmith/OpensourcererSpaceMouse.git", // git location of the library
+//	                      "v2-frame.stl" , // file to load
+//	                      null)// no parameters (see next tutorial)
 
-CSG title = CSG.text("Opensourcerer",5.0, 30.0, "FreeSerif")
-			.movey(40)
+CSG title = CSG.text("Opensourcerer",5.0, 16, "Alef")
+			.rotx(-90)
+title = title.movex(title.
+			//.movey(40)
 
-CSG ret = ref.union(title)
+//return[title, ref]
